a13a92306315ab57d577848d35da61b3fe652da7
julia <89081314528@mail.ru>
2022-01-31 17:16:38 +0300

    add lesson 9

---
+++ lesson-9-1-spring-rest/src/main/java/ru/geekbrains/rest/CategoryResource.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rest/category")
+public class CategoryResource {
+}
