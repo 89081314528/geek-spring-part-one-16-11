259550db966ec03f09e07bcf546005617fce5795
julia <89081314528@mail.ru>
2022-01-31 18:43:34 +0300

    add lesson 9

--- lesson-9-1-spring-rest/src/main/java/ru/geekbrains/service/CategoryServiceImpl.java
+++ lesson-9-1-spring-rest/src/main/java/ru/geekbrains/service/CategoryServiceImpl.java
@@ -9,9 +9,6 @@ import javax.xml.catalog.Catalog;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private static CategoryDto convertToDto(Category category) {
-        return new CategoryDto(category.getId(), category.getName());
-    }
 
     CategoryRepository categoryRepository;
 
